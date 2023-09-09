9d255064e83a08a832ffd74192065a71f51ac543
YarynaHuk <jasmin.huk@gmail.com>
2023-09-09 14:13:09 +0200

    added project spa-center

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById('open-menu-btn').addEventListener('click', function(){
+    document.getElementsByClassName('open-menu-holder')[0].classList.toggle('open');
+});
+
+document.getElementById('close-menu-btn').addEventListener('click', function(){
+    document.getElementsByClassName('open-menu-holder')[0].classList.toggle('open');
+});
